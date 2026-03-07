cc3e592711121bbd234d7db864f7bb42b73aa1ec
Serhii0080 <startap.chrom@gmail.com>
2026-03-08 01:45:30 +0200

    task HW1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './index';
 import { HW1 } from './HW/1/HW1';
 import { HW2 } from './HW/2/HW2';
 import { HW3 } from './HW/3/HW3';
@@ -7,7 +7,7 @@ import { HW4 } from './HW/4/HW4';
 function App() {
   return (
     <div className="App">
-      {/*<HW1 />*/}
+      <HW1 />
       {/*<HW2 />*/}
       {/*<HW3 />*/}
       {/*<HW4 />*/}
--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -1,5 +1,15 @@
 import { UserList } from './UserList';
 
+export type User = {
+	id: number
+	name: string
+	age: number
+	address: {
+		street: string;
+		city: string;
+	};
+}
+
 export const HW1 = () => {
 
 	// 1️⃣ Раскомментируйте JSX (HW1.tsx, UserList.tsx) и вы увидите,
@@ -14,7 +24,7 @@ export const HW1 = () => {
 	// ❗ id на элементах не удаляем и не изменяем! Они нужны для автотестов.
 	// Если нужно подставить элемент массива в id, то подставляем по аналогии с другими элементами!
 
-	const users = [
+	const users: User[] = [
 		{ id: 1, name: 'John', age: 25, address: { street: '123 Main St', city: 'New York' } },
 		{ id: 2, name: 'Alice', age: 30, address: { street: '456 Elm St', city: 'San Francisco' } },
 		{ id: 3, name: 'Bob', age: 35, address: { street: '789 Oak St', city: 'Seattle' } },
@@ -34,7 +44,7 @@ export const HW1 = () => {
 
 	return (
 		<div id={'hw01'}>
-			<UserList users={'НАЗВАНИЕ МАССИВА, КОТОРЫЙ МЫ ПЕРЕДАЕМ'} />
+			<UserList users={users} />
 		</div>
 	);
 };
