cb48cc11ca7aba2fe3ee96b5d7980cafb0af7ee8
Serhii0080 <startap.chrom@gmail.com>
2026-03-30 03:06:57 +0300

    task HW4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,17 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (currentText: string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		// НУЖНО ДОПИСАТЬ
+		const text = event.currentTarget.value
+		props.setCurrentText(text)
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+		<input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
 };
